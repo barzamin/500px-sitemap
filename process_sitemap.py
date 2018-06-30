a32b92687404f30cb82bb1692597c693eea189fe
Erin Moon <erin@hashbang.sh>
2018-06-30 17:34:21 -0500

    get idx

--- process_sitemap.py
+++ process_sitemap.py
@@ -1,6 +1,7 @@
 import sys
 import gzip
 from lxml import etree
+import re
 
 filename = sys.argv[1]
 if filename.endswith('gz'):
@@ -10,4 +11,14 @@ else:
     with open(filename) as f:
         root = etree.parse(f)
 
-print(root)
+urlset = root.getroot()
+
+RE_URL = re.compile(r'https?\://500px.com/photo/(\d+)')
+
+for url_e in urlset:
+    loc_e = url_e.find('{*}loc')
+    uri = loc_e.text
+    uri_match = RE_URL.match(uri)
+    assert uri_match is not None
+
+    idx = uri_match.group(1)
