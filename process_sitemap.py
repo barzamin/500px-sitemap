ab24f9d4f236d30f5284be1c9743b4a0473dbf55
Erin Moon <erin@hashbang.sh>
2018-06-30 17:40:18 -0500

    done

--- process_sitemap.py
+++ process_sitemap.py
@@ -17,8 +17,13 @@ RE_URL = re.compile(r'https?\://500px.com/photo/(\d+)')
 
 for url_e in urlset:
     loc_e = url_e.find('{*}loc')
-    uri = loc_e.text
-    uri_match = RE_URL.match(uri)
+    uri_match = RE_URL.match(loc_e.text)
     assert uri_match is not None
 
     idx = uri_match.group(1)
+
+
+    license_e = url_e.find('{*}image').find('{*}license')
+    license_uri = license_e.text
+    if 'creativecommons' in license_uri:
+        print(idx)        
