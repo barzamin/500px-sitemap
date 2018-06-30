355da820674e5551deff1207b6b51fcf7558bbdf
Erin Moon <erin@hashbang.sh>
2018-06-30 17:18:31 -0500

    fix typo

--- process_sitemap.py
+++ process_sitemap.py
@@ -3,9 +3,11 @@ import gzip
 from lxml import etree
 
 filename = sys.argv[1]
-if filename.endswith('gz')
+if filename.endswith('gz'):
     with gzip.open(filename) as f:
         root = etree.parse(f)
 else:
     with open(filename) as f:
         root = etree.parse(f)
+
+print(root)
